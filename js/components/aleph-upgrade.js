c7b84f52da65fbe4e74584424f5b09568d71ca7d
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-05-23 16:34:59 +0100

    Update aleph-upgrade.js

--- js/components/aleph-upgrade.js
+++ js/components/aleph-upgrade.js
@@ -1,4 +1,4 @@
 Vue.component("aleph-upgrade", {
     props: ["upgrade"],
-    template: `<resource-upgrade :upgrade="upgrade" :resourcename="'<span class=` + 'aleph' + `>ℵ</span>'"></resource-upgrade>`
+    template: `<resource-upgrade :upgrade="upgrade" :resourcename="'<img alt="NC" class="inline" src="images/N-Coin.png"/>'"></resource-upgrade>`
 });
